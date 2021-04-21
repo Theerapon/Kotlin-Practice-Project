f8d210796515c8963d9f8d75d95a13216b3de317
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-04-22 02:02:33 +0700

    initial Spring Boot

---
+++ src/main/kotlin/com/example/kotlin/springboot/practice/PracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.springboot.practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeApplication>(*args)
+}
